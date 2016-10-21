525b6ecc80baf668f593d9fa4b1ab823d278abec
Mohammed Khalil <fthedex@gmail.com>
2016-10-21 09:56:25 +0300

    finished permissions
    <3

--- ControlPanel.php
+++ ControlPanel.php
@@ -24,7 +24,7 @@ exit();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="Styles/Khalil.css">
-   <!-- <script src="Scripts/controlPanelEvents.js"></script> -->
+
 
     <?php
     if($globalUser->loggedIn()){
@@ -523,7 +523,7 @@ font-family: 'Anton', sans-serif;font-size:24px;margin-top:5px;" href="Login.php
     </div>";
        }
        else{  //student
-
+           echo "<div style='padding: 0px;' id='studentPanelBoard' class='container-fluid boxShadow'></div>";
        }
     }
 
