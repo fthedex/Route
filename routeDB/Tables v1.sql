e8a8beda51eb48a134f6d59f58f62ba5e88930fb
Mohammed Ayyad <mohammedayyad25@gmail.com>
2016-11-05 16:19:45 +0200

    Updated version 2.0
    Does not contain tables for :
    Admin.
    Assistant.
    BusRoundTime look-up table.
    Table for bus rounds history (csv or log file may be used instead)

--- routeDB/Tables v1.sql
+++ routeDB/Tables v1.sql
@@ -2,74 +2,86 @@ create table accountInfo
 (
 	accountInfoID int not null,
     accountPassword varchar(255) not null,
-    accountType int null,
+    accountType int not null,
     primary key(accountInfoID)
-)
-
+) ENGINE = InnoDB;
+-------------------------------------------------------
 create table bus
 (
 	busID int auto_increment not null,
     busModel varchar(25) null,
     busModelYear date null,
-    busCapacity int null,
-    busIdleTime date null,
+    busCapacity int not null,
+    busIdleTime date not null,
     primary key(busID)
-)
+) ENGINE = InnoDB;; 
 
+
+alter table bus auto_increment = 200001;
+-------------------------------------------------------
 create table line
 (
-	lineID int not null,
-    lineName varchar(25) null,
+	lineID int not null auto_increment,
+    lineName varchar(25) not null,
+    lineDesc varchar(50) null,
     primary key(lineID)
-);
+) ENGINE = InnoDB;
 
-create table busRound
+alter table line auto_increment = 1000001;
+-------------------------------------------------------
+create table updateBusLocation
 (
-	roundID int auto_increment not null,
-    busID int null,
-    lineID int null,
-    startTime date null,
-    finishTime date null,
-    primary key(roundID),
-    foreign key(busID) references bus(busID) ON UPDATE CASCADE ON DELETE SET NULL,
-    foreign key(lineID) references line(lineID) ON UPDATE CASCADE ON DELETE SET NULL
-);
-
-
-create table hotspot
+	busID int not null,
+    busLong double(50, 30) not null,
+    busLati double(50, 30) not null,
+    primary key(busID),
+    foreign key(busID) references bus(busID) ON UPDATE CASCADE
+) ENGINE = InnoDB;
+--------------------------------------------------------
+create table student
 (
-	hotspotID int auto_increment not null,
-    lineID int null,
-    longitude decimal(10, 6) null,
-    latitude decimal(10, 6) null,
-    primary key(hotspotID),
-    foreign key(lineID) references line(lineID) ON UPDATE CASCADE ON DELETE SET NULL
-);
+	studentID int auto_increment not null,
+    accountType int not null,
+    studentFN varchar(25) null,
+    studentLN varchar(25) null,
+    studentGender varchar(1) null,
+    studentDoB date null,
+    studentGrade varchar(2) null,
+    studentBus int null,
+    primary key(studentID),
+    foreign key(studentBus) references bus(busID) ON UPDATE CASCADE
+)ENGINE = InnoDB;
 
-create table busRoundTime
+alter table student auto_increment = 201600001;
+-------------------------------------------------------
+create table parent
 (
-	roundID int not null,
-    hotspotID int not null,
-    EAT time null,
-    primary key(roundID, hotspotID),
-    foreign key(roundID) references busRound(roundID) ON UPDATE CASCADE,
-    foreign key(hotspotID) references hotspot(hotspotID) ON UPDATE CASCADE
-);
+	parentID int auto_increment not null,
+    accountType int not null,
+    parentFN varchar(25) null,
+    parentLN varchar(25) null,
+    parentDoB date null,
+    parentGender varchar(1) null,
+    parentNumber varchar(25) not null,
+    parentAddress varchar(50) null,
+    primary key(parentID)
+)ENGINE = InnoDB;
 
-create table busRoundHistory
+alter table parent auto_increment = 201610001;
+---------------------------------------------------------
+create table studentParent
 (
-	roundID int not null,
-    hotspotID int not null,
-    roundDate date null,
-    EAT time null,
-    AAT time null,
-    primary key(roundID, hotspotID)
-);
-
+	parentID int not null,
+    studentID int not null,
+    primary key(parentID, studentID),
+    foreign key(parentID) references parent(parentID),
+    foreign key(studentID) references student(studentID)
+)ENGINE = InnoDB;
+---------------------------------------------------------
 create table driver
 (
     driverID int auto_increment not null,
-    accountType int null,
+    accountType int not null,
     driverFN varchar(25) null,
     driverLN varchar(25) null,
     driverGender varchar(1) null,
@@ -81,52 +93,34 @@ create table driver
     primary key(driverID),
     foreign key(driverBusID) references bus(busID) ON UPDATE CASCADE ON DELETE SET NULL,
     foreign key(driverlineID) references line(lineID) ON UPDATE CASCADE ON DELETE SET NULL
-);
+)ENGINE = InnoDB;
 
-
-create table updateBusLocation
+alter table driver auto_increment = 300001;
+---------------------------------------------------------
+create table hotspot
 (
-	busID int not null,
-    busLong float(10,6) not null,
-    busLati float(10,6) not null,
-    primary key(busID),
-    foreign key(busID) references bus(busID) ON UPDATE CASCADE
-);
+	hotspotID int auto_increment not null,
+    lineID int null,
+    longitude decimal(10, 6) null,
+    latitude decimal(10, 6) null,
+    primary key(hotspotID),
+    foreign key(lineID) references line(lineID) ON UPDATE CASCADE ON DELETE SET NULL
+)ENGINE = InnoDB;
 
-create table parent
+alter table hotspot auto_increment = 3000001;
+--------------------------------------------------------
+create table busRound
 (
-	parentID int auto_increment not null,
-    accountType int null,
-    parentFN varchar(25) null,
-    parentLN varchar(25) null,
-    parentDoB date null,
-    parentGender varchar(1) null,
-    parentNumber varchar(25) null,
-    parentAddress varchar(50) null,
-    primary key(parentID)
-);
+	roundID int auto_increment not null,
+    busID int null,
+    lineID int null,
+    startTime date null,
+    finishTime date null,
+    primary key(roundID),
+    foreign key(busID) references bus(busID) ON UPDATE CASCADE ON DELETE SET NULL,
+    foreign key(lineID) references line(lineID) ON UPDATE CASCADE ON DELETE SET NULL
+)ENGINE = InnoDB;
 
-create table student
-(
-	studentID int auto_increment not null,
-    accountType int null,
-    studentFN varchar(25) null,
-    studentLN varchar(25) null,
-    studentGender varchar(1) null,
-    studentDoB date null,
-    studentGrade varchar(2) null,
-    studentRound int null,
-    primary key(studentID),
-    foreign key(studentRound) references busRound(roundID) ON UPDATE CASCADE
-);
-
-
-create table studentParent
-(
-	parentID int not null,
-    studentID int not null,
-    primary key(parentID, studentID),
-    foreign key(parentID) references parent(parentID),
-    foreign key(studentID) references student(studentID)
-);
+alter table busRound auto_increment = 2000001;
+--------------------------------------------------------
 
